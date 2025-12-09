2c2eef89c4e4a613d3a03dc2abeb2e2a85c5af6f
Guido <guido@gyoza.es>
2025-12-09 16:37:27 +0100

    Add favicon

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/',
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      'interuniversity-supereffluent-gwyn.ngrok-free.dev', // el host de ngrok
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
